6b7302ba6a8d45f0df80d36f3e70d0021352b64d
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-22 07:59:30 -0800

    update db table and db name

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   password: "labber",
   host: "localhost",
   port: 5432,
-  database: "jwttutorial"
+  database: "authtodolist"
 });
 
 module.exports = pool;
--- server/db.sql
+++ server/db.sql
@@ -3,14 +3,23 @@ CREATE DATABASE jwttutorial;
 -- download extension
 -- create extension if not exists "uuid-ossp";
 CREATE TABLE users(
-  user_id uuid PRIMARY KEY DEFAULT
-  uuid_generate_v4(),
+  user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   user_name VARCHAR(255) NOT NULL,
-  user_email VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
   user_password VARCHAR(255) NOT NULL
 );
 
+
+CREATE TABLE todos(
+  todo_id SERIAL,
+  user_id UUID,
+  description VARCHAR(255),
+  PRIMARY KEY(todo_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 -- insert fake users
 
 INSERT INTO users (user_name, user_email, user_password )
 VALUES ('Hope', 'hopevaughnwarren@gmail.com', 'hvw1980');
+
+INSERT INTO todos(user_id, description) VALUES ('fccd1e68-35c4-4e6f-954a-98b521d96760', 'clean room');
